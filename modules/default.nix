a56846c3fdc401b08293eb2c91a27b47b81d2a2e
kmolski <krzysztof.molski29@gmail.com>
2024-02-08 18:29:48 +0100

    lang(default): Change default language to English (US)

--- modules/default.nix
+++ modules/default.nix
@@ -2,7 +2,18 @@
 
 {
   time.timeZone = "Europe/Warsaw";
-  i18n.defaultLocale = "pl_PL.UTF-8";
+  i18n.defaultLocale = "en_US.UTF-8";
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "pl_PL.UTF-8";
+    LC_IDENTIFICATION = "pl_PL.UTF-8";
+    LC_MEASUREMENT = "pl_PL.UTF-8";
+    LC_MONETARY = "pl_PL.UTF-8";
+    LC_NAME = "pl_PL.UTF-8";
+    LC_NUMERIC = "pl_PL.UTF-8";
+    LC_PAPER = "pl_PL.UTF-8";
+    LC_TELEPHONE = "pl_PL.UTF-8";
+    LC_TIME = "pl_PL.UTF-8";
+  };
 
   programs.fish.enable = true;
   environment.systemPackages = with pkgs; [
