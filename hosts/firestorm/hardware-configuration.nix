b6b8901eb5b87ceea5b2358842f91151f78c9fe9
kmolski <krzysztof.molski29@gmail.com>
2024-02-09 16:28:55 +0100

    fix(firestorm): Shrink swapfile to 4GiB

--- hosts/firestorm/hardware-configuration.nix
+++ hosts/firestorm/hardware-configuration.nix
@@ -21,7 +21,7 @@
     };
 
   swapDevices =
-    [{ device = "/var/lib/swapfile"; size = 8192; }];
+    [{ device = "/var/lib/swapfile"; size = 4096; }];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
