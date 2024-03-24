bc1fa05198157e989065ee581641091678d66686
kmolski <krzysztof.molski29@gmail.com>
2024-03-24 10:23:09 +0100

    refactor(cups-server): Open cupsd firewall ports

--- modules/cups-server.nix
+++ modules/cups-server.nix
@@ -4,6 +4,7 @@
   services.printing = {
     enable = true;
     browsing = true;
+    openFirewall = true;
     allowFrom = [ "all" ];
     listenAddresses = [ "*:631" ];
     drivers = [ pkgs.carps-cups ];
