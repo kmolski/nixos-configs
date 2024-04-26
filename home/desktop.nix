6907ac9382c77bfb3c4501cf8d6222f7adc7ebfa
kmolski <krzysztof.molski29@gmail.com>
2024-04-26 21:41:55 +0200

    feat(gpg): Add GPG configuration with SSH agent

--- home/desktop.nix
+++ home/desktop.nix
@@ -4,19 +4,53 @@
   imports = [ ./minimal.nix ];
 
   home.packages = with pkgs; [
+    # Web browsers
     chromium
     firefox
+
+    # Productivity
     jetbrains.idea-ultimate
     libreoffice
+    virt-manager
+
+    # Multimedia
     obs-studio
     spotify
-    virt-manager
     vlc
+
+    # Fonts
+    fira
+    iosevka
   ];
+
+  programs.gpg = {
+    enable = true;
+    settings = {
+      keyid-format = "0xlong";
+      with-fingerprint = true;
+      s2k-cipher-algo = "AES256";
+      s2k-digest-algo = "SHA512";
+      cert-digest-algo = "SHA512";
+      throw-keyids = true;
+      no-symkey-cache = true;
+      require-cross-certification = true;
+      personal-cipher-preferences = [ "AES256" "AES192" "AES" ];
+      personal-digest-preferences = [ "SHA512" "SHA384" "SHA256" ];
+      personal-compress-preferences = [ "ZLIB" "BZIP2" "ZIP" "Uncompressed" ];
+      default-preference-list = [ "SHA512" "SHA384" "SHA256" "AES256" "AES192" "AES" "ZLIB" "BZIP2" "ZIP" "Uncompressed" ];
+    };
+  };
 
   programs.direnv = {
     enable = true;
     nix-direnv.enable = true;
+  };
+
+  services.gpg-agent = {
+    enable = true;
+    pinentryFlavor = "qt";
+    enableSshSupport = true;
+    sshKeys = [ "AE473752B03BB459" ];
   };
 
   nixpkgs.config.allowUnfree = true;
