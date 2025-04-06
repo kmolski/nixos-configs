03ac6f75862c3c0d1aaca3e6fcfa2834e5964997
kmolski <krzysztof.molski29@gmail.com>
2025-04-06 09:14:16 +0200

    chore(secrets): Add S3 secret for cloudburst backup

--- hosts/secrets/secrets.nix
+++ hosts/secrets/secrets.nix
@@ -1,8 +1,11 @@
 let
+  cloudburst = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHZ8nu/+jgTjV+/QUXXHDaYq+Achcd384zP1cIDCJ9cJ" ];
   firestorm = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIG+WDpBm2B0VhFyYzzXgV/SzBMpHv4j/XqOBx4l5h/kI" ];
   rainbow = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBKOXV2GzMUDEZP2cIwvCkL9O7BIx+sJkUJKjz6Dyb9/" ];
 in
 {
+  "backup-cloudburst-key.age".publicKeys = cloudburst;
+
   "duckdns-token.age".publicKeys = firestorm;
   "wg-privatekey.age".publicKeys = firestorm;
 
